81b9a8c26f94ce158c257fc4f95daae4168a079c
edudallatorre <eduardodallatorre.io@gmail.com>
2023-03-01 21:31:59 +0000

    Created MVC

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
+const RegisterController = require('./controllers/RegisterController')
 
 const PORT = process.env.PORT || 8000
 
@@ -16,17 +17,33 @@ app.get('/', (req, res) => {
     res.send('Hello from nodemon')
 })
 
+<<<<<<< Updated upstream
 try {
     mongoose.connect(process.env.MONGO_DB_CONNECTION, {
         useNewUrlParser: true,
         userUnifiedTopology: true,
+=======
+app.get('/register', (req, res) => {
+    
+})
+
+app.post('/register', RegisterController.store)
+
+try {
+    mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+>>>>>>> Stashed changes
     })
     console.log('MongoDB connected')
 } catch (error) {
     console.log(error)
 }
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
